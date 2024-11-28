ae0c8562cea82f3ef05ba8a0ae0cd8049097b202
MartinSchoeler <martinschoeler8@gmail.com>
2024-11-28 17:08:27 -0300

    chore: remove meteor.startup from reaction-message-action

--- apps/meteor/client/components/message/toolbar/MessageToolbar.tsx
+++ apps/meteor/client/components/message/toolbar/MessageToolbar.tsx
@@ -15,6 +15,7 @@ import { usePermalinkStar } from './usePermalinkStar';
 import { useStarMessageAction } from './useStarMessageAction';
 import { useUnstarMessageAction } from './useUnstarMessageAction';
 import { useWebDAVMessageAction } from './useWebDAVMessageAction';
+import { useReactionMessageAction } from '../../../../app/reactions/client/useReactionMessageAction';
 import type { MessageActionContext } from '../../../../app/ui-utils/client/lib/MessageAction';
 import { MessageAction } from '../../../../app/ui-utils/client/lib/MessageAction';
 import { useEmojiPickerData } from '../../../contexts/EmojiPickerContext';
@@ -94,6 +95,7 @@ const MessageToolbar = ({
 	useStarMessageAction(message, { room, user });
 	useUnstarMessageAction(message, { room, user });
 	usePermalinkStar(message, { subscription, user });
+	useReactionMessageAction(message, { user, room, subscription });
 
 	const actionsQueryResult = useQuery({
 		queryKey: roomsQueryKeys.messageActionsWithParameters(room._id, message),
