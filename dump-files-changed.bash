01753810a7dfb2110d0baf34096e012bac4ce85d
aviv <avivbs2468@gmail.com>
2025-01-30 11:31:54 +0200

    added helpful script for PR

---
+++ dump-files-changed.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+# ReadMe: This Script will copy all your PR changed files to a tar.gz and zip, while keeping folder hierarchy
+
+# Make Sure you are on the PRed branch
+changed_files=$(git diff --name-only hichat)
+timestamp=$(date +%Y%m%d)
+
+mkdir pr_changed_files
+for file in $changed_files; do
+	echo "Changed file: $file"
+	mkdir -p $(dirname ./pr_changed_files/$file) && touch $file
+	echo $file > ./pr_changed_files/$file
+done
+
+targz_name="pr_changed_files_${timestamp}.tar.gz"
+zip_name="pr_changed_files_${timestamp}.zip"
+
+tar -czf  "$targz_name" pr_changed_files --force-local
+zip -r -o "$zip_name" pr_changed_files
+rm -rf pr_changed_files
+echo "Created: $targz_name"
+echo "Created: $zip_name"
+
+
