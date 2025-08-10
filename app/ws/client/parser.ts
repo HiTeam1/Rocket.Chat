4baa825565a199296caa204b182d4b24dfd94c96
LeeorShalev <leeorShalev15@gmail.com>
2025-08-10 13:22:26 +0300

    fix

--- app/ws/client/parser.ts
+++ app/ws/client/parser.ts
@@ -3,39 +3,6 @@
 import { Packet } from "socket.io-parser";
 import { EventEmitter } from "events";
 import { serializer } from "/app/serialization/serializer";
-
-// export const  customParser = {
-
-//   encode :(packet: Packet) => {
-//     try {
-//       const serializedData = serializer.serialize(packet);
-//       return [serializedData];
-//     } catch (error) {
-//       console.error('Encoding error:', error);
-//       return ['{"type": "error", "data": "encoding_failed"}'];
-//     }
-//   },
-
-//   decode :(data: any) => {
-//     try {
-//       const packet = serializer.deserialize(data);
-//       if (packet === null) {
-//         return [{
-//           type: 'error',
-//           data: 'deserialization_failed'
-//         }];
-//       }
-
-//       return [packet];
-//     } catch (error) {
-//       console.error('Decoding error:', error);
-//       return [{
-//         type: 'error',
-//         data: 'decoding_failed'
-//       }];
-//     }
-//   }
-// }
 export class Encoder {
   encode(packet: Packet): any[] {
     return [serializer.serialize(packet)];
